4c2c63eb64febb690f79cb4b2915c1e9d5e221ce
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-09-24 14:34:41 +0200

    feat(ui): add launch button for Rust game server
    - Introduced a new Button component in HomePage to launch the Rust game server.
    - Integrated a link to connect to the server directly from the UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="font-extrabold text-5xl tracking-tight sm:text-[5rem]">
+    <main className='flex min-h-screen flex-col items-center justify-center'>
+      <div className='container flex flex-col items-center justify-center gap-12 px-4 py-16'>
+        <h1 className='font-extrabold text-5xl tracking-tight sm:text-[5rem]'>
           Rustroam
         </h1>
+
+        <Button asChild variant='shine' size='lg'>
+          <Link href='steam://run/252490//+connect 161.97.147.247:28015'>
+            Launch Rust
+          </Link>
+        </Button>
       </div>
     </main>
   );
