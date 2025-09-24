b07bb49b6727bd3d2146fb0da30ba4fe664b98bb
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-09-24 14:32:32 +0200

    feat(rust): add getServerState function for querying Rust server state
    - Implemented a new function to query the state of a Rust game server using the gamedig library.
    - The function handles both successful responses and errors appropriately.

---
+++ src/lib/rust.ts
@@ -0,0 +1,15 @@
+import { GameDig } from "gamedig";
+
+export async function getServerState(host: string, port: number) {
+  return await GameDig.query({
+    type: "rust",
+    host,
+    port,
+  })
+    .then((state) => {
+      return state;
+    })
+    .catch((error) => {
+      return error;
+    });
+}
