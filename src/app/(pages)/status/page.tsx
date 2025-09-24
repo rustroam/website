3d21e8bf49511cfb07001ec74179f9f12d18944f
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-09-25 00:13:02 +0200

    chore(status): comment out server state fetching in ServerStatusPage
    - Temporarily commented out the server state fetching logic and error handling in ServerStatusPage.
    - Removed the display of server state details for debugging purposes.

--- src/app/(pages)/status/page.tsx
+++ src/app/(pages)/status/page.tsx
@@ -1,22 +1,22 @@
-import { SERVER_DETAILS } from "@/consts/rust";
-import { getServerState } from "@/rust/query";
+/* import { SERVER_DETAILS } from "@/consts/rust";
+import { getServerState } from "@/rust/query"; */
 
 export default async function ServerStatusPage() {
-  const serverState = await getServerState(
+  /* const serverState = await getServerState(
     SERVER_DETAILS.host,
     SERVER_DETAILS.port
   );
 
   if (serverState instanceof Error) {
     return <div>Error: {serverState.message}</div>;
-  }
+  } */
 
   return (
     <main className="relative flex min-h-[calc(100dvh-5rem)] flex-col items-center justify-center">
       <div className="container relative z-10 flex flex-col items-center justify-center gap-8 px-4 py-16">
         ServerStatusPage
       </div>
-      <pre>{JSON.stringify(serverState, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(serverState, null, 2)}</pre> */}
     </main>
   );
 }
