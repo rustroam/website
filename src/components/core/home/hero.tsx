66bb4b2357e14741773b038ca76e378a5042363d
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-09-25 21:23:50 +0200

    feat(hero): add animated text effect to hero component
    - Wrapped the hero text in a new TextAnimate component for enhanced visual appeal.
    - Introduced TextAnimate component to handle various text animation effects.

--- src/components/core/home/hero.tsx
+++ src/components/core/home/hero.tsx
@@ -1,7 +1,11 @@
+import { TextAnimate } from "@/components/ui/text-animate";
+
 export function Hero() {
   return (
     <h1 className='font-extrabold text-5xl tracking-tight sm:text-[5rem]'>
-      rustroam
+      <TextAnimate animation='blurInUp' by='character' duration={0.5}>
+        rustroam
+      </TextAnimate>
     </h1>
   );
 }
