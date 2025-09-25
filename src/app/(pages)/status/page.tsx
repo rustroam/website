83e6d292e195f447081c445235db9ad735d1201a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-09-25 02:28:26 +0200

    fix(status): update ServerStatusPage message to indicate upcoming feature

--- src/app/(pages)/status/page.tsx
+++ src/app/(pages)/status/page.tsx
@@ -14,7 +14,7 @@ export default async function ServerStatusPage() {
   return (
     <main className="relative flex min-h-[calc(100dvh-5rem)] flex-col items-center justify-center">
       <div className="container relative z-10 flex flex-col items-center justify-center gap-8 px-4 py-16">
-        ServerStatusPage
+        coming soon
       </div>
       {/* <pre>{JSON.stringify(serverState, null, 2)}</pre> */}
     </main>
